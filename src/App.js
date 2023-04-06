c416df9e3f54eac121f5a4e814be26f9108ddc48
Felix Ondullah <felixondullah@gmail.com>
2023-04-06 10:44:35 +0300

    added the assets and worked on roots

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
-import {Route, Routes} from 'react-dom';
+import {Route, Routes } from "react-router-dom";
 import {Box} from "@mui/material";
+import ExcerciseDetails from "./Components/pages/ExcerciseDetails";
+import Home from "./Components/pages/Home";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
-    <Box>
+    <Box width = "400px" sx = {{width: {x1:'1488px'}}} m = "auto">
       <Navbar />
       <Routes>
-        <Route path= "/" element= {Home />} />
-        <Route path= "/exercise/:id" element= {ExerciseDetail />} />
+        <Route path= "/" element= {<Home/>} />
+        <Route path= "/exercise/:id" element= {<ExcerciseDetails />} />
       </Routes>
+      <Footer/>
     </Box>
   
   );
